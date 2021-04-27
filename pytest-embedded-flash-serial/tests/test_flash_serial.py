4444155cf5f28779a67c7cb29add6522bc0f6f4d
Fu Hanxi <fuhanxi@espressif.com>
2021-04-27 15:32:24 +0800

    test(base): add tests for class App

--- pytest-embedded-flash-serial/tests/test_flash_serial.py
+++ pytest-embedded-flash-serial/tests/test_flash_serial.py
@@ -1,14 +1,22 @@
+import os
+
+PLUGINS = [
+    '-p', 'pytest_embedded',
+    '-p', 'pytest_embedded_flash_jtag',
+    '-p', 'pytest_embedded_flash_serial',
+]
+
+
 def test_flash(testdir):
     testdir.makepyfile("""
-        def test_printf(capsys, dut):
+        def test_flash_serial(capsys, dut):
             dut.flash()
             stdout, _ = capsys.readouterr()
             assert stdout.strip() == 'Flashed by serial'
     """)
 
     result = testdir.runpytest(
-        '-p', 'pytest_embedded',
-        '-p', 'pytest_embedded_flash_jtag',
-        '-p', 'pytest_embedded_flash_serial',
+        *PLUGINS,
+        '--app-path', os.path.join(testdir.tmpdir, 'hello_world'),
     )
     assert result.ret == 0
--- pytest-embedded/tests/test_base.py
+++ pytest-embedded/tests/test_base.py
@@ -1,3 +1,5 @@
+import os
+
 PLUGINS = [
     '-p', 'pytest_embedded',
 ]
@@ -16,15 +18,24 @@ def test_help(testdir):
 
 def test_fixtures(testdir):
     testdir.makepyfile("""
+        import logging
+
         def test_fixtures_test_file_name(test_file_name):
             assert test_file_name == 'test_fixtures'
 
         def test_fixtures_test_case_name(test_case_name):
             assert test_case_name == 'test_fixtures_test_case_name'
-    """)
 
+        def test_fixtures_app(app):
+            assert len(app.flash_files) == 3
+            assert type(app.partition_table) == dict
+            assert type(app.sdkconfig) == dict
+    """)
+    testdir.chdir()
     result = testdir.runpytest(
-        *PLUGINS
+        *PLUGINS,
+        '--app-path', os.path.join(testdir.tmpdir, 'hello_world'),
+        '--part-tool', os.path.join(testdir.tmpdir, 'gen_esp32part.py'),
     )
 
-    result.assert_outcomes(passed=2)
+    result.assert_outcomes(passed=3)
