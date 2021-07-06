788addb7be4edd6c4d0876da424365188d35bd6c
Fu Hanxi <fuhanxi@espressif.com>
2021-07-06 16:30:37 +0800

    feat(serial): extract serial into a standalone fixture

--- pytest-embedded-serial/pytest_embedded_serial/dut.py
+++ pytest-embedded-serial/pytest_embedded_serial/dut.py
@@ -1,76 +1,30 @@
-import multiprocessing
 from typing import Optional
 
-import serial
+import pexpect
 from pytest_embedded.app import App
 from pytest_embedded.dut import Dut
-from pytest_embedded.log import to_str
+
+from .serial import Serial
 
 
 class SerialDut(Dut):
     """
     Dut class for serial ports
 
-    :ivar: port: serial port string
-    :ivar: port_inst: :class:`pyserial.Serial` instance
-    :ivar: forward_io_proc: A process which forward the serial output to :attr:`pexpect_proc` stdin
+    :ivar: serial: :class:`Serial` instance
+    :ivar: app: :class:`pytest_embedded.app.App` instance
+    :ivar: pexpect_proc: :class:`pexpect.spawn` instance
     """
 
-    DEFAULT_PORT_CONFIG = {
-        'baudrate': 115200,
-        'bytesize': serial.EIGHTBITS,
-        'parity': serial.PARITY_NONE,
-        'stopbits': serial.STOPBITS_ONE,
-        'timeout': 0.05,
-        'xonxoff': False,
-        'rtscts': False,
-    }
+    def __init__(
+        self, serial: Serial = None, app: Optional[App] = None, pexpect_proc: Optional[pexpect.spawn] = None, **kwargs
+    ) -> None:
+        super().__init__(app, pexpect_proc, **kwargs)
 
-    def __init__(self, app: Optional[App] = None, port: Optional[str] = None, *args, **kwargs) -> None:
-        super().__init__(app, *args, **kwargs)
+        self.serial = serial
+        self.serial.create_forward_io_process(self.pexpect_proc, source='serial')
 
-        if port is None:
-            raise ValueError('please specify port')
-        else:
-            self.port = port
-
-        self.port_config = self.DEFAULT_PORT_CONFIG
-        self.port_inst = self._open_port_session()
-        self._start()
-
-        # forward_io_proc would get output from ``open_port_session``, do some pre-process jobs and then forward the
-        # the output to the ``pexpect_proc``, which only accept type str as input
-        self.forward_io_proc = self._open_forward_io_process()
-        self.forward_io_proc.start()
-
-        self._sessions_close_methods.extend(
-            [
-                self.forward_io_proc.terminate,
-                self.port_inst.close,
-            ]
-        )
-
-    def _start(self):
-        pass
-
-    def _open_port_session(self) -> serial.Serial:
-        return serial.serial_for_url(self.port, **self.port_config)
-
-    def _open_forward_io_process(self) -> multiprocessing.Process:
-        proc = multiprocessing.Process(target=self._forward_io)
-        return proc
-
-    @Dut.redirect_stdout('serial')
-    def _forward_io(self, breaker: bytes = b'\n'):
-        while True:
-            line = b''
-            sess_output = self.port_inst.read()  # a single char
-            while sess_output and sess_output != breaker:
-                line += sess_output
-                sess_output = self.port_inst.read()
-            line += sess_output
-            line = to_str(line)
-            print(line)
+        self.proc_close_methods.append(self.serial.close)
 
     def write(self, data: bytes) -> int:
-        return self.port_inst.write(data)
+        return self.serial.proc.write(data)
--- pytest-embedded-serial/pytest_embedded_serial/plugin.py
+++ pytest-embedded-serial/pytest_embedded_serial/plugin.py
@@ -1,40 +1,56 @@
-import logging
 from types import ModuleType
 
 import pytest
 
 from .dut import SerialDut
-
-
-def pytest_addoption(parser):
-    group = parser.getgroup('embedded')
-    group.addoption('--port', help='serial port. Could be overridden by pytest parametrizing. (Default: "auto")')
+from .serial import Serial
 
 
 @pytest.fixture
 def port(request):
     """
-    Apply parametrization to fixture :func:`pytest_embedded_serial.plugin.dut`
+    Apply parametrization to fixture :func:`pytest_embedded_serial.plugin.serial`
     """
     return {'port': getattr(request, 'param', None)}
 
 
 @pytest.fixture
-def dut(port, app, options) -> SerialDut:
+def serial(port, pexpect_proc, options):
+    """
+    Uses :attr:`options['Serial']` as kwargs to create instance.
+
+    :return: :class:`pytest_embedded_serial.serial.Serial` instance
+    """
+    serial_options = options.get('Serial', {})
+    if port['port']:
+        serial_options.update(port)
+    serial_options['pexpect_proc'] = pexpect_proc
+
+    serial = Serial(**serial_options)
+    try:
+        yield serial
+    finally:
+        serial.close()
+
+
+@pytest.fixture
+def dut(serial, app, pexpect_proc, options) -> SerialDut:
     """
     Uses :attr:`options['Dut']` as kwargs to create instance.
 
-    :return: :class:`pytest_embedded.dut.Dut` or derived class instance
+    :return: :class:`pytest_embedded_serial.dut.SerialDut` instance
     """
     dut_options = options.get('Dut', {})
-    if port['port']:
-        dut_options.update(port)
-    logging.info(dut_options)
-    dut = SerialDut(app=app, **dut_options)
+    dut = SerialDut(serial, app, pexpect_proc, **dut_options)
     try:
         yield dut
     finally:
         dut.close()
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('embedded')
+    group.addoption('--port', help='serial port. Could be overridden by pytest parametrizing. (Default: "None")')
 
 
 @pytest.hookimpl
@@ -42,4 +58,5 @@ def pytest_plugin_registered(plugin, manager):
     if not isinstance(plugin, ModuleType) or plugin.__name__ != 'pytest_embedded.plugin':
         return
 
-    plugin.KNOWN_OPTIONS['Dut'].append('port')
+    plugin.KNOWN_OPTIONS['Serial'].append('port')
+    plugin.ENV['serial'] = True
