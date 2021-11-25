ba97eaa5805b29a11177641bfbe39d7554bd3dc1
Fu Hanxi <fuhanxi@espressif.com>
2021-11-25 12:31:21 +0800

    feat: add `expect_list` and `expect_exact`

--- pytest-embedded/pytest_embedded/dut.py
+++ pytest-embedded/pytest_embedded/dut.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Match, Optional
+from typing import AnyStr, Match, Optional, Union
 
 import pexpect
 
@@ -33,10 +33,15 @@ class Dut(ProcessContainer):
         """
         self.pexpect_proc.write(*args, **kwargs)
 
-    def expect(self, *args, **kwargs) -> Match:
+    def expect(self, *args, **kwargs) -> Union[Match, AnyStr]:
         """
-        Expect from `pexpect_proc`. All arguments would pass to `pexpect.expect()`. Return the current `re.Match`
-        object if matched.
+        Expect from `pexpect_proc`. All the arguments would pass to `pexpect.expect()`.
+
+        Returns:
+            AnyStr: if you're matching pexpect.EOF or pexpect.TIMEOUT to get all the current buffers.
+
+        Returns:
+            re.Match: if matched given string.
         """
         try:
             self.pexpect_proc.expect(*args, **kwargs)
@@ -44,4 +49,54 @@ class Dut(ProcessContainer):
             logging.error(f'Not found {args}, {kwargs}')
             raise
         else:
+            if self.pexpect_proc.match in [pexpect.EOF, pexpect.TIMEOUT]:
+                return self.pexpect_proc.before.rstrip()
+
+            return self.pexpect_proc.match
+
+    def expect_exact(self, *args, **kwargs) -> Union[Match, AnyStr]:
+        """
+        Expect from `pexpect_proc`. All the arguments would pass to `pexpect.expect_exact()`.
+
+        Returns:
+            AnyStr: if you're matching pexpect.EOF or pexpect.TIMEOUT to get all the current buffers.
+
+        Returns:
+            re.Match: if matched given string.
+        """
+        try:
+            self.pexpect_proc.expect_exact(*args, **kwargs)
+        except (pexpect.EOF, pexpect.TIMEOUT):
+            logging.error(f'Not found {args}, {kwargs}')
+            raise
+        else:
+            if self.pexpect_proc.match in [pexpect.EOF, pexpect.TIMEOUT]:
+                return self.pexpect_proc.before.rstrip()
+
+            return self.pexpect_proc.match
+
+    def expect_list(self, *args, **kwargs) -> Union[Match, AnyStr]:
+        """
+        Expect from `pexpect_proc`. All the arguments would pass to `pexpect.expect_list()`.
+
+        Returns:
+            AnyStr: if you're matching pexpect.EOF or pexpect.TIMEOUT to get all the current buffers.
+
+        Returns:
+            re.Match: if matched given compiled regex.
+
+        Notes:
+            For the first argument, which is the regex list, you should pass the compiled regex with bytes.
+
+            For example, could pass `[re.compile(b'foo'), re.compile(b'bar')]`
+        """
+        try:
+            self.pexpect_proc.expect_list(*args, **kwargs)
+        except (pexpect.EOF, pexpect.TIMEOUT):
+            logging.error(f'Not found {args}, {kwargs}')
+            raise
+        else:
+            if self.pexpect_proc.match in [pexpect.EOF, pexpect.TIMEOUT]:
+                return self.pexpect_proc.before.rstrip()
+
             return self.pexpect_proc.match
