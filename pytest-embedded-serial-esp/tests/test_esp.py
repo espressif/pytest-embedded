772e1abe5793ff9fc0ccae04268e1751e55eed3d
Fu Hanxi <fuhanxi@espressif.com>
2022-03-15 15:52:17 +0800

    ci: use 32 and c3 runner only

--- pytest-embedded-serial-esp/tests/test_esp.py
+++ pytest-embedded-serial-esp/tests/test_esp.py
@@ -5,7 +5,7 @@ def test_detect_port(testdir):
     testdir.makepyfile(
         """
         def test_detect_port(dut):
-            assert dut[0].serial.target == 'esp32s2'
+            assert dut[0].serial.target == 'esp32c3'
             assert dut[1].serial.target == 'esp32'
     """
     )
@@ -14,7 +14,7 @@ def test_detect_port(testdir):
         '-s',
         '--count', 2,
         '--embedded-services', 'esp',
-        '--target', 'esp32s2|esp32',
+        '--target', 'esp32c3|esp32',
     )
 
     result.assert_outcomes(passed=1)
@@ -24,11 +24,11 @@ def test_detect_port_with_cache(testdir, caplog, first_index_of_messages):
     testdir.makepyfile(
         """
         def test_detect_port(dut):
-            assert dut[0].serial.target == 'esp32s2'
+            assert dut[0].serial.target == 'esp32c3'
             assert dut[1].serial.target == 'esp32'
 
         def test_detect_port_again(dut):
-            assert dut[0].serial.target == 'esp32s2'
+            assert dut[0].serial.target == 'esp32c3'
             assert dut[1].serial.target == 'esp32'
     """
     )
@@ -37,21 +37,21 @@ def test_detect_port_with_cache(testdir, caplog, first_index_of_messages):
         '-s',
         '--count', 2,
         '--embedded-services', 'esp',
-        '--target', 'esp32s2|esp32',
+        '--target', 'esp32c3|esp32',
         '--log-cli-level', 'DEBUG',
     )
 
     result.assert_outcomes(passed=2)
 
-    esp32s2_set_cache_index = first_index_of_messages(
-        re.compile('^set port-target cache: .+ - esp32s2$', re.MULTILINE), caplog.messages
+    esp32c3_set_cache_index = first_index_of_messages(
+        re.compile('^set port-target cache: .+ - esp32c3$', re.MULTILINE), caplog.messages
     )
     esp32_set_cache_index = first_index_of_messages(
-        re.compile('^set port-target cache: .+ - esp32$', re.MULTILINE), caplog.messages, esp32s2_set_cache_index + 1
+        re.compile('^set port-target cache: .+ - esp32$', re.MULTILINE), caplog.messages, esp32c3_set_cache_index + 1
     )
-    esp32s2_hit_cache_index = first_index_of_messages(
-        re.compile('^hit port-target cache: .+ - esp32s2$', re.MULTILINE), caplog.messages, esp32_set_cache_index + 1
+    esp32c3_hit_cache_index = first_index_of_messages(
+        re.compile('^hit port-target cache: .+ - esp32c3$', re.MULTILINE), caplog.messages, esp32_set_cache_index + 1
     )
     first_index_of_messages(
-        re.compile('^hit port-target cache: .+ - esp32$', re.MULTILINE), caplog.messages, esp32s2_hit_cache_index + 1
+        re.compile('^hit port-target cache: .+ - esp32$', re.MULTILINE), caplog.messages, esp32c3_hit_cache_index + 1
     )
