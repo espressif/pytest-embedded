ee3b03386ddbb43ec02b241bebc0d066152e9316
Fu Hanxi <fuhanxi@espressif.com>
2022-09-19 17:41:32 +0800

    fix gdb wrong method

--- pytest-embedded-jtag/pytest_embedded_jtag/gdb.py
+++ pytest-embedded-jtag/pytest_embedded_jtag/gdb.py
@@ -21,34 +21,34 @@ class Gdb(DuplicateStdoutPopen):
         """
         GDB/MI commands -interpreter-exec console [cmd]
         """
-        self.send(f'-interpreter-exec console "{cmd}"')
+        self.write(f'-interpreter-exec console "{cmd}"')
 
     def gdb_set(self, *args):
         """
         GDB/MI commands `-gdb-set ...`
         """
-        self.send(f'-gdb-set {" ".join(args)}')
+        self.write(f'-gdb-set {" ".join(args)}')
 
     def gdb_exit(self):
         """
         GDB/MI commands `-gdb-exit`
         """
-        self.send('-gdb-exit')
+        self.write('-gdb-exit')
 
     def file_exec_and_symbols(self, filepath: str):
         """
         GDB/MI commands `-file-exec-and-symbols [filepath]`
         """
-        self.send(f'-file-exec-and-symbols "{filepath}"')
+        self.write(f'-file-exec-and-symbols "{filepath}"')
 
     def break_insert(self, location):
         """
         GDB/MI commands `-break-insert [location]`
         """
-        self.send(f'-break-insert {location}')
+        self.write(f'-break-insert {location}')
 
     def exec_continue_all(self):
         """
         GDB/MI commands `-exec-continue --all`
         """
-        self.send('-exec-continue --all')
+        self.write('-exec-continue --all')
