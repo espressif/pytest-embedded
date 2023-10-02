b368f0fa38d82cd20c5f70bdc5cd1204499d2b71
Uri Shaked <uri@urishaked.com>
2023-10-02 22:49:21 +0300

    fix(wokwi): don't overwrite wokwi.toml and diagram.json

--- pytest-embedded-wokwi/pytest_embedded_wokwi/wokwi_cli.py
+++ pytest-embedded-wokwi/pytest_embedded_wokwi/wokwi_cli.py
@@ -1,8 +1,10 @@
 import json
+import logging
 import os
 import typing as t
 from pathlib import Path
 
+import toml
 from pytest_embedded import __version__
 from pytest_embedded.log import DuplicateStdoutPopen
 
@@ -40,32 +42,9 @@ class WokwiCLI(DuplicateStdoutPopen):
             wokwi_cli_path: Wokwi CLI arguments
         """
         self.app = app
-        flasher_args = Path(app.binary_path, 'flasher_args.json')
 
-        with open(os.path.join(app.app_path, 'wokwi.toml'), 'wt') as f:
-            f.write(
-                f"""
-[wokwi]
-version = 1
-generatedBy = 'pytest-embedded-wokwi {__version__}'
-firmware = '{Path(flasher_args).relative_to(app.app_path).as_posix()}'
-elf = '{Path(app.elf_file).relative_to(app.app_path).as_posix()}'
-"""
-            )
-
-        # TODO: Check if diagram already exist and update it?
-        diagram = {
-            'version': 1,
-            'author': 'Uri Shaked',
-            'editor': 'wokwi',
-            'parts': [{'type': target_to_board[app.target], 'id': 'esp'}],
-            'connections': [
-                ['esp:TX', '$serialMonitor:RX', '', []],
-                ['esp:RX', '$serialMonitor:TX', '', []],
-            ],
-        }
-        with open(os.path.join(app.app_path, 'diagram.json'), 'wt') as f:
-            f.write(json.dumps(diagram))
+        self.create_wokwi_toml()
+        self.create_diagram_json()
 
         wokwi_cli = wokwi_cli_path or self.wokwi_cli_executable
 
@@ -77,6 +56,67 @@ elf = '{Path(app.elf_file).relative_to(app.app_path).as_posix()}'
     @property
     def wokwi_cli_executable(self):
         return self.WOKWI_CLI_PATH
+
+    def create_wokwi_toml(self):
+        app = self.app
+        flasher_args = Path(app.binary_path, 'flasher_args.json')
+        wokwi_toml_path = os.path.join(app.app_path, 'wokwi.toml')
+        firmware_path = Path(flasher_args).relative_to(app.app_path).as_posix()
+        elf_path = Path(app.elf_file).relative_to(app.app_path).as_posix()
+
+        if os.path.exists(wokwi_toml_path):
+            with open(wokwi_toml_path, 'rt') as f:
+                toml_data = toml.load(f)
+
+            if 'wokwi' not in toml_data:
+                toml_data['wokwi'] = {'version': 1}
+
+            wokwi_table = toml_data['wokwi']
+            if wokwi_table.get('firmware') == firmware_path and wokwi_table.get('elf') == elf_path:
+                # No need to update
+                return
+
+            wokwi_table.update({'firmware': firmware_path, 'elf': elf_path})
+        else:
+            toml_data = {
+                'wokwi': {
+                    'version': 1,
+                    'generatedBy': f'pytest-embedded-wokwi {__version__}',
+                    'firmware': firmware_path,
+                    'elf': elf_path,
+                }
+            }
+
+        with open(wokwi_toml_path, 'wt') as f:
+            toml.dump(toml_data, f)
+
+    def create_diagram_json(self):
+        app = self.app
+        diagram_json_path = os.path.join(app.app_path, 'diagram.json')
+        target_board = target_to_board[app.target]
+
+        if os.path.exists(diagram_json_path):
+            with open(diagram_json_path, 'rt') as f:
+                json_data = json.load(f)
+            if not any(part['type'] == target_board for part in json_data['parts']):
+                logging.warning(
+                    f'diagram.json exists, no part with type "{target_board}" found. '
+                    + 'You may need to update the diagram.json file manually to match the target board.'
+                )
+            return
+
+        diagram = {
+            'version': 1,
+            'author': 'Uri Shaked',
+            'editor': 'wokwi',
+            'parts': [{'type': target_board, 'id': 'esp'}],
+            'connections': [
+                ['esp:TX', '$serialMonitor:RX', ''],
+                ['esp:RX', '$serialMonitor:TX', ''],
+            ],
+        }
+        with open(diagram_json_path, 'wt') as f:
+            f.write(json.dumps(diagram, indent=2))
 
     def _hard_reset(self):
         """
