1387af520a241d7a007bb3205f940a88cea2d15a
Fu Hanxi <fuhanxi@espressif.com>
2024-04-29 18:29:18 +0000

    fix(qemu): stop require optional dependency 'idf'

--- pytest-embedded-qemu/pytest_embedded_qemu/__init__.py
+++ pytest-embedded-qemu/pytest_embedded_qemu/__init__.py
@@ -1,11 +1,27 @@
 """Make pytest-embedded plugin work with QEMU."""
 
+import importlib
+
+from pytest_embedded.utils import lazy_load
+
 DEFAULT_IMAGE_FN = 'flash_image.bin'
 ENCRYPTED_IMAGE_FN = f'encrypted_{DEFAULT_IMAGE_FN}'
 
-from .app import QemuApp  # noqa
 from .dut import QemuDut  # noqa
 from .qemu import Qemu  # noqa
+
+
+__getattr__ = lazy_load(
+    importlib.import_module(__name__),
+    {
+        'Qemu': Qemu,
+        'QemuDut': QemuDut,
+    },
+    {
+        'QemuApp': '.app',  # requires idf
+    },
+)
+
 
 __all__ = [
     'DEFAULT_IMAGE_FN',
