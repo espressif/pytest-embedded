e7db6687adf6a01925ab6772491f4c7fb467e25b
Fu Hanxi <fuhanxi@espressif.com>
2024-04-30 09:42:31 +0200

    fix(arduino): stop require optional dependency 'esp'

--- pytest-embedded-arduino/pytest_embedded_arduino/__init__.py
+++ pytest-embedded-arduino/pytest_embedded_arduino/__init__.py
@@ -1,11 +1,22 @@
 """Make pytest-embedded plugin work with Arduino."""
 
-from .app import ArduinoApp
-from .serial import ArduinoSerial
+import importlib
 
-__all__ = [
-    'ArduinoApp',
-    'ArduinoSerial',
-]
+from pytest_embedded.utils import lazy_load
+
+from .app import ArduinoApp
+
+__getattr__ = lazy_load(
+    importlib.import_module(__name__),
+    {
+        'ArduinoApp': ArduinoApp,
+    },
+    {
+        'ArduinoSerial': '.serial',  # requires esp
+    },
+)
+
+__all__ = ['ArduinoApp', 'ArduinoSerial']
+
 
 __version__ = '1.10.0'
